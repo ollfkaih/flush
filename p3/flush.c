4e4ec61eb516dca2e8c7ed99bf7f59c52b9d729f
Olav Kihle <olavki@stud.ntnu.no>
2022-03-31 15:33:59 +0200

    Stream issues

--- p3/flush.c
+++ p3/flush.c
@@ -15,6 +15,7 @@
 
 void inputPrompt(void);
 void handleInput(char input[MAX_ARGS][MAX_PATH]);
+void clearInputBuffer(void);
 
 char currentDirectory[MAX_PATH] = "";
 
@@ -74,18 +75,85 @@ void ls(char dir[MAX_PATH]) {
         }
         (void) closedir(directory);
     } else {
-        printf("Unable to open dir\n");
+        printf("[ls] Unable to open dir\n");
     }
 }
 
 void cd(char dir[MAX_PATH]) {
     if (strlen(dir) > 0) {
         if (chdir(&dir[0]) != 0) {
-            printf("Unable to change directory to %s\n", &dir[0]);
+            printf("[cd] Unable to change directory to %s\n", &dir[0]);
         } 
     } else {
         // change to home dir (not required)
         chdir(getenv("HOME"));
+    }
+}
+
+void forkAndExec(char command[MAX_ARGS][MAX_PATH]) {
+    // get stdin descriptor
+
+
+    pid_t pid = fork();
+    int status;
+
+    if (pid < 0) {
+        printf("Error: unable to fork\n");
+    }
+    if (pid == 0) {
+        if (strcmp(command[0], "ls") == 0) {
+            ls(command[1]);
+            exit(0);
+        } else if (strcmp(command[0], "pwd") == 0) {
+            printf("%s\n", currentDirectory);
+            exit(0);
+        } else if (strcmp(command[0], "clear") == 0 || strcmp(command[0], "cls") == 0) {
+            printf("\033[2J\033[1;1H");
+            exit(0);
+        } else if (strcmp(command[0], "help") == 0) {
+            printf("ls - list files in current directory\n");
+            printf("cd - change directory\n");
+            printf("pwd - print current directory\n");
+            printf("clear - clear screen\n");
+            printf("jobs - prints all running background processes\n");
+            printf("exit - exit program\n");
+            exit(0);
+        } else if (strcmp(command[0],"cd") != 0) {
+            char *args[MAX_ARGS];
+            args[0] = command[0];
+            for (int i = 1; i < MAX_ARGS; i++) {
+                if (strlen(command[i]) > 0) {
+                    args[i] = command[i];
+                } else {
+                    args[i] = NULL;
+                }
+            }
+            args[MAX_ARGS - 1] = NULL;
+            execvp(args[0], args);
+
+            // if execvp fails, print error
+            printf("Error: unable to execute %s\n", args[0]);
+            exit(0);
+        }
+    } else if (pid > 0) {
+        printf("%d", pid);
+        waitpid(-1, &status, 0);
+
+        if ( WIFEXITED(status) ) {
+            // set stdin and stdout to tty
+            freopen("/dev/tty", "r", stdin);
+            freopen("/dev/tty", "w", stdout);     
+
+            // close stdin
+            //close(STDIN_FILENO);
+            // close stdout
+            //close(STDOUT_FILENO);
+
+            int es = WEXITSTATUS(status);
+            printf("Exit status [", es);
+            printArgs(command);
+            printf("] was %d\n", es);
+        }
     }
 }
 
@@ -100,57 +168,67 @@ void handleInput(char input[MAX_ARGS][MAX_PATH]) {
     } else if (strcmp(input[0], "cd") == 0) {
         cd(input[1]);
     }
-    
-    pid_t pid = fork();
-    int status;
 
-    if (pid < 0) {
-        printf("Error: unable to fork\n");
-    }
-    if (pid == 0) {
-        if (strcmp(input[0], "ls") == 0) {
-            ls(input[1]);
-            exit(0);
-        } else if (strcmp(input[0], "pwd") == 0) {
-            printf("%s\n", currentDirectory);
-            exit(0);
-        } else if (strcmp(input[0], "clear") == 0 || strcmp(input[0], "cls") == 0) {
-            printf("\033[2J\033[1;1H");
-            exit(0);
-        } else if (strcmp(input[0], "help") == 0) {
-            printf("ls - list files in current directory\n");
-            printf("cd - change directory\n");
-            printf("pwd - print current directory\n");
-            printf("clear - clear screen\n");
-            printf("jobs - prints all running background processes\n");
-            printf("exit - exit program\n");
-            exit(0);
-        } else if (strcmp(input[0],"cd") != 0) {
-            char *args[MAX_ARGS];
-            args[0] = input[0];
-            for (int i = 1; i < MAX_ARGS; i++) {
-                if (strlen(input[i]) > 0) {
-                    args[i] = input[i];
-                } else {
-                    args[i] = NULL;
+    freopen("/dev/tty", "r", stdin);
+    freopen("/dev/tty", "w", stdout);
+
+    // loop over input and check for &, ;, <, >, |
+    // if &, fork and exec
+    // if ;, exec
+    // if <, redirect stdin
+    // if >, redirect stdout
+    // if |, fork and exec
+    // if no redirection, exec
+    int execFlag = 0;
+    char command[MAX_ARGS][MAX_PATH];
+    memset(&command, 0, sizeof(command));
+    int commandIndex = 0;
+    
+    for (int i = 0; i < MAX_ARGS && strlen(input[i]) > 0; i++) {
+        if (strcmp(input[i], "&") == 0) {
+            printf("Found &\n");
+            execFlag = 1;
+            // fork and exec
+        } else if (strcmp(input[i], ";") == 0) {
+            printf("Found ;\n");
+            execFlag = 1;
+            // exec
+        } else if (strcmp(input[i], "<") == 0) {
+            printf("Found <\n");
+            // redirect stdin
+            for (int j = i + 1; j < MAX_ARGS; j++) {
+                if (strlen(input[j]) > 0) {
+                    execFlag = 1;
+                    freopen(input[j], "r", stdin);
+                    break;
                 }
             }
-            args[MAX_ARGS - 1] = NULL;
-            execvp(args[0], args);
-
-            // if execvp fails, print error
-            printf("Error: unable to execute %s\n", args[0]);
-            exit(0);
+        } else if (strcmp(input[i], ">") == 0) {
+            printf("Found >\n");
+            // redirect stdout
+            for (int j = i + 1; j < MAX_ARGS; j++) {
+                if (strlen(input[j]) > 0) {
+                    execFlag = 1;
+                    freopen(input[j], "w", stdout);
+                    break;
+                }
+            }
+        } else if (strcmp(input[i], "|") == 0) {
+            execFlag = 1;
+            printf("Found |\n");
+            // fork and exec
+        } else {
+            if (execFlag) {
+                commandIndex = 0;
+                execFlag = 0;
+            }
+            strcpy(&command[commandIndex][0], input[i]);
+            commandIndex++;
         }
-    } else if (pid > 0) {
-        
-        waitpid(-1, &status, 0);
 
-        if ( WIFEXITED(status) ) {
-            int es = WEXITSTATUS(status);
-            printf("Exit status [", es);
-            printArgs(input);
-            printf("] was %d\n", es);
-        }
+        if (execFlag)
+            forkAndExec(command);
     }
+    // exec last command entered
+    forkAndExec(command);
 }
